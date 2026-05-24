08c959b2bfce72b0ff9b7f3c785da99ef6523369
Heeseung <iameeeom@gmail.com>
2026-05-24 23:46:29 +0900

    Modify Code for compile error

--- src/components/wedding/Gallery.tsx
+++ src/components/wedding/Gallery.tsx
@@ -1,14 +1,4 @@
-import photo1 from "@/assets/1.jpg"; 
-import photo2 from "@/assets/2.jpg";
-import photo3 from "@/assets/3.jpg";
-import photo4 from "@/assets/4.jpg";
-import photo5 from "@/assets/5.jpg";
-import photo6 from "@/assets/6.jpg";
-import photo7 from "@/assets/7.jpg";
-import photo8 from "@/assets/8.jpg";
-
-// 이렇게 사진들을 하나의 배열로 묶어두면 편해요!
-const photos = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8];
+const photos = ["/1.jpg", "/2.jpg", "/3.jpg", "/4.jpg", "/5.jpg", "/6.jpg", "/7.jpg", "/8.jpg"];
 
 export function Gallery() {
   return (
