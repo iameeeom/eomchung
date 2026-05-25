207ddb54c59a924d7f0b2c50652c3db903b3463d
Heeseung <iameeeom@gmail.com>
2026-05-25 20:25:54 +0900

    update

--- src/components/wedding/Location.tsx
+++ src/components/wedding/Location.tsx
@@ -89,7 +89,7 @@ export function Location() {
         </a>
 
         <a
-          href="https://map.kakao.com/?urlX=509316&urlY=1107616&itemId=&q=%EB%A3%A8%EC%9D%B4%EB%B9%84%EC%8A%A4%EC%BB%A8%EB%B2%A4%EC%85%98&srcid=&map_type=TYPE_MAP"
+          href="https://map.kakao.com/?urlX=525939.999999998&urlY=1106942.9999999993&urlLevel=3&itemId=1862136403&q=%EB%A3%A8%EC%9D%B4%EB%B9%84%EC%8A%A4%EC%BB%A8%EB%B2%A4%EC%85%98%20%EC%86%A1%ED%8C%8C%EB%AC%B8%EC%A0%95%EC%A0%90&srcid=1862136403&map_type=TYPE_MAP"
           target="_blank" rel="noreferrer"
           className="flex-1 py-3 rounded-xl bg-lime border border-lime/20 text-lime-foreground text-sm hover:opacity-90 transition text-center flex items-center justify-center font-medium shadow-md"
         >
